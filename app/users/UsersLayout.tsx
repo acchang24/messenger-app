c17d66b79b868cc58d9a6c52f7fa5e9a6dbc4a36
acchang24 <acchang24@gmail.com>
2023-10-25 04:48:32 -0700

    userlist error

--- app/users/UsersLayout.tsx
+++ app/users/UsersLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
